bacd347575c16f1264d31f233cb0a5cf23dfc556
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-23 20:19:28 -0500

    fix spinner to wait for axios

--- api/sendemail/index.js
+++ api/sendemail/index.js
@@ -1,5 +1,5 @@
 const nodemailer = require('nodemailer');
-const { SmsClient } = require('@azure/communication-sms');
+// const { SmsClient } = require('@azure/communication-sms');
 
 module.exports = async function (context, req) {
 //   const { DefaultAzureCredential } = require('@azure/identity');
@@ -21,7 +21,7 @@ module.exports = async function (context, req) {
     },
   });
 
-  const smsClient = new SmsClient('endpoint=https://kffsmscs.communication.azure.com/;accesskey=zM9uliUzojQ+GFjq6/Hewxe82vaLXsFPodOE3im6/wr1AggXC9+SiMYGufTOKM+bKDNYcj3OGXKYJ+FqdyewJA==');
+  // const smsClient = new SmsClient('endpoint=https://kffsmscs.communication.azure.com/;accesskey=zM9uliUzojQ+GFjq6/Hewxe82vaLXsFPodOE3im6/wr1AggXC9+SiMYGufTOKM+bKDNYcj3OGXKYJ+FqdyewJA==');
   
   const mailOptions = {
     from: 'kaifamilyfarm123@outlook.com',
@@ -48,12 +48,12 @@ module.exports = async function (context, req) {
       console.log(error);
     } else {
         console.log('Sent: ' + info.response);
-        smsClient.send({
-            from: "+18444021959",
-            to: ["+14699106366", "+14695250450"],
-            // to: ["+14699106366", "+14057610896"],
-            message: "Order from  " + req.body.emailAddress + "... " + req.body.orderTotal
-        });
+        // smsClient.send({
+        //     from: "+18444021959",
+        //     to: ["+14699106366", "+14695250450"],
+        //     // to: ["+14699106366", "+14057610896"],
+        //     message: "Order from  " + req.body.emailAddress + "... " + req.body.orderTotal
+        // });
     }
   });
 };
